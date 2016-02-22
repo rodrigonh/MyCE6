f0dccd51b3d856602cd5d01f38eed0b570be987a
Sinri Edogawa <e.joshua.s.e@gmail.com>
2016-02-22 10:01:12 +0800

    Network and Threading jobs

---
+++ SmartDeviceProject1/SmartDeviceProject1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace SmartDeviceProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.menuItem4 = new System.Windows.Forms.MenuItem();
+            this.menuItem5 = new System.Windows.Forms.MenuItem();
+            this.menuItem6 = new System.Windows.Forms.MenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusBar1 = new System.Windows.Forms.StatusBar();
+            this.menuItem7 = new System.Windows.Forms.MenuItem();
+            this.menuItem8 = new System.Windows.Forms.MenuItem();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItem1);
+            this.mainMenu1.MenuItems.Add(this.menuItem3);
+            this.mainMenu1.MenuItems.Add(this.menuItem7);
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.MenuItems.Add(this.menuItem2);
+            this.menuItem1.Text = "邪恶的大鲵";
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Text = "关于";
+            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+            // 
+            // menuItem3
+            // 
+            this.menuItem3.MenuItems.Add(this.menuItem4);
+            this.menuItem3.MenuItems.Add(this.menuItem5);
+            this.menuItem3.MenuItems.Add(this.menuItem6);
+            this.menuItem3.Text = "网络测试（本地）";
+            // 
+            // menuItem4
+            // 
+            this.menuItem4.Text = "Freedom首页";
+            this.menuItem4.Click += new System.EventHandler(this.menuItem4_Click);
+            // 
+            // menuItem5
+            // 
+            this.menuItem5.Text = "POST TEST";
+            this.menuItem5.Click += new System.EventHandler(this.menuItem5_Click);
+            // 
+            // menuItem6
+            // 
+            this.menuItem6.Text = "Async GET";
+            this.menuItem6.Click += new System.EventHandler(this.menuItem6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(371, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "让我们伸出触手！";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(28, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(586, 21);
+            this.label1.Text = "人类真是虚无！在下面随便写点字然后按下按钮。世界就变得美好了。";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(28, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(586, 203);
+            this.label2.Text = "此处应有网络访问请求";
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.Location = new System.Drawing.Point(0, 431);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(638, 24);
+            this.statusBar1.Text = "statusBar1";
+            // 
+            // menuItem7
+            // 
+            this.menuItem7.MenuItems.Add(this.menuItem8);
+            this.menuItem7.Text = "Thread测试";
+            // 
+            // menuItem8
+            // 
+            this.menuItem8.Text = "可怕的触手之一";
+            this.menuItem8.Click += new System.EventHandler(this.menuItem8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(638, 455);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "CE测试程序 - Sinri";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.MenuItem menuItem4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuItem menuItem5;
+        private System.Windows.Forms.MenuItem menuItem6;
+        private System.Windows.Forms.StatusBar statusBar1;
+        private System.Windows.Forms.MenuItem menuItem7;
+        private System.Windows.Forms.MenuItem menuItem8;
+    }
+}
+
---
+++ SmartDeviceProject1/SmartDeviceProject1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace SmartDeviceProject1
+{
+    public partial class Form1 : Form
+    {
+        private HttpWebResponse sharedResponse;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("观测到输入的文本是："+this.textBox1.Text);
+        }
+
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "作为一只大鲵，必须要邪恶。";
+        }
+
+        private void menuItem4_Click(object sender, EventArgs e)
+        {
+            string userName = "userName";
+            string tagUrl = "https://freedom.everstray.com/";
+            EasyNeter easy_neter=new EasyNeter();
+            //CookieCollection cookies = new CookieCollection();//如何从response.Headers["Set-Cookie"];中获取并设置CookieCollection的代码略  
+            HttpWebResponse response = easy_neter.getHttp(tagUrl);//.CreateGetHttpResponse(tagUrl, null, null, cookies);
+            this.textBox1.Text = "Http Status Code = " + response.StatusCode;
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            String content = sr.ReadToEnd();
+            this.label2.Text = content;
+        }
+
+        private void menuItem5_Click(object sender, EventArgs e)
+        {
+            string tagUrl = "http://sinri.tk/StringLDJudge";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("str1", "apple");
+            parameters.Add("str2", "pear");
+            EasyNeter easy_neter = new EasyNeter();
+            HttpWebResponse response = easy_neter.postHttp(tagUrl, parameters, 30, null, Encoding.UTF8);
+            this.textBox1.Text = "Http Status Code = " + response.StatusCode;
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            String content = sr.ReadToEnd();
+            this.label2.Text = content;
+        }
+
+        private void menuItem6_Click(object sender, EventArgs e)
+        {
+            string tagUrl = "https://freedom.everstray.com/";
+
+            Dictionary<string, object> state = new Dictionary<string, object>();
+            state.Add("target", this);
+            state.Add("url", tagUrl);
+
+            bool async_gone=EasyAsync.register(new WaitCallback(Form1.callback_1),state);
+            this.textBox1.Text=(async_gone?"Async Begins...":"Async failed to begin!");
+        }
+
+        private static void callback_1(object state)
+        {
+            try
+            {
+                Dictionary<string, object> dict = (Dictionary<string, object>)state;
+                EasyNeter easy_neter = new EasyNeter();
+                object oform;
+                object ourl;
+                dict.TryGetValue("target", out oform);
+                dict.TryGetValue("url", out ourl);
+                Form1 form = (Form1)oform;
+                string url = (string)ourl;
+
+                HttpWebResponse response = easy_neter.getHttp(url);
+                form.sharedResponse = response;
+                /*
+                form.statusBar1.Text = "URL: " + url;
+                HttpWebResponse response = easy_neter.getHttp(url);//.CreateGetHttpResponse(tagUrl, null, null, cookies);
+                form.textBox1.Text = "Http Status Code = " + response.StatusCode;
+                StreamReader sr = new StreamReader(response.GetResponseStream());
+                String content = sr.ReadToEnd();
+                form.label2.Text = content;
+                */
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                MessageBox.Show(e.StackTrace.ToString());
+            }
+        }
+        
+        // 声明一个委托 
+        private delegate void NewDel();
+
+        private void menuItem8_Click(object sender, EventArgs e)
+        {
+            Kaishi();
+        }
+
+        // 创建一个 新线程的方法
+        public void Kaishi()
+        {
+            Thread thread;
+            ThreadStart threadstart = new ThreadStart(start);
+            thread = new Thread(threadstart);
+            thread.Start();
+        }
+
+        // 屏蔽错误的方法   说白了 就是通过了一个 委托  
+        // 解决Control.Invoke 必须用于与在独立线程上创建的控件交互。
+        private void start()
+        {
+            if (InvokeRequired)
+            {
+                // 要 努力 工作的 方法
+                BeginInvoke(new NewDel(GetMac));
+            }
+        }
+        // 实质工作的 方法体
+        private void GetMac()
+        {
+            string userName = "userName";
+            string tagUrl = "https://freedom.everstray.com/";
+            EasyNeter easy_neter = new EasyNeter();
+            HttpWebResponse response = easy_neter.getHttp(tagUrl);
+            this.textBox1.Text = "Http Status Code = " + response.StatusCode;
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            String content = sr.ReadToEnd();
+            this.label2.Text = content;
+        }
+    }
+}
